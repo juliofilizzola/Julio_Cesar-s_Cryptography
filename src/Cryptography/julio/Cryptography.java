ca05c411c0a556f0aa7cdc1f9dee0c0bb6665b3a
Filizzola <juliofilizzola@hotmail.com>
2021-12-08 11:33:08 -0300

    BUILD: finalizando metodo encrypting"

--- src/Cryptography/julio/Cryptography.java
+++ src/Cryptography/julio/Cryptography.java
@@ -1,11 +1,21 @@
 package Cryptography.julio;
 
 public class Cryptography {
-  public String Cryptography(String text) throws IllegalAccessException {
+  public String Encrypting(String text) throws IllegalAccessException {
     if (text.equals("")) throw new IllegalAccessException("Error");
     String formatText = text.toLowerCase();
     char[] latters = formatText.toCharArray();
 
     String encryptedText = "";
+
+    for (char latter :  latters) {
+      if (latter != ' ' && !Character.isDigit(latter)) {
+        int characterASCII = latter + 3;
+        encryptedText += (char)characterASCII;
+      } else {
+        encryptedText += latter;
+      }
+    }
+    return encryptedText;
   }
 }
